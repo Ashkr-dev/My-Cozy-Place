5a69ffc53913588280c428e0b3796e8e6673a5c1
Ashkr-dev <ashkr2657@gmail.com>
2025-10-23 13:46:18 +0530

    reduce the size of gui controls for small devices

--- src/script.js
+++ src/script.js
@@ -263,6 +263,7 @@ controls.maxDistance = 20;
 controls.update();
 
 if (window.innerWidth < 768) {
+  gui.width = "100px";
   camera.position.set(8.221521463083404, 7.027146726208887, 10.037595818238497);
   controls.target.set(
     0.2749239606428338,
